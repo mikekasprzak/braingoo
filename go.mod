fbb82dfca717f3fef7e314328656be7c1de06538
Mike Kasprzak <mike@distraction.engineer>
2024-11-18 15:34:52 -0500

    dummy calling the children

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module braingoo
 
 go 1.23.3
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import "fmt"
+import "time"
+import "braingoo/wellknown"
+import "braingoo/api"
 
 func main() {
-    fmt.Println("Hello, world.")
+    fmt.Println("Hello, world. ", time.Now(), api.Instance());
+
+	wellknown.NodeInfo();
 }
 
