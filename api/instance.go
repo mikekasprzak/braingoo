5040cf1e1d260584203808cd19f77262350ba3c2
Mike Kasprzak <mike@distraction.engineer>
2024-11-18 15:33:18 -0500

    stubbed in sub-packages

---
+++ api/instance.go
@@ -0,0 +1,8 @@
+package api
+
+// https://docs.joinmastodon.org/methods/instance/
+// https://fedidb.org/crawler.html
+
+func Instance() int {
+	return 1
+}
---
+++ wellknown/nodeinfo.go
@@ -0,0 +1,10 @@
+package wellknown
+
+import "fmt"
+
+// https://github.com/jhass/nodeinfo/tree/main
+// https://fedidb.org/crawler.html
+
+func NodeInfo() {
+	fmt.Printf("Node\n")
+}
